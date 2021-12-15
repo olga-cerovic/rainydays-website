434cb1ced9bd59439da428c894ad6739d703e913
olga-cerovic <olga.cerovic@stud.noroff.no>
2021-12-16 00:12:27 +0100

    Html/ css/ js
    Created new js files, updated html and css files

---
+++ js/close-product.js
@@ -0,0 +1,7 @@
+const productAside = document.querySelector(".product-aside");
+
+const closeProductDetails = () => {
+  const closeProduct = document.querySelector(".close-product");
+  productAside.style.display = "none";
+  console.log("pljah");
+};
---
+++ js/open-product.js
@@ -0,0 +1,7 @@
+const productAsideOpening = document.querySelector(".product-aside");
+
+const openProductDetails = () => {
+  const openProduct = document.querySelector(".open-product");
+  productAsideOpening.style.display = "block";
+  console.log("pljahahaha");
+};
