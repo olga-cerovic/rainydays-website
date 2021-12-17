89fb72a1a16e0702c4bc61c998cda7ded403c06a
olga-cerovic <olga.cerovic@stud.noroff.no>
2021-12-17 09:42:45 +0100

    Html
    Created new files

--- js/close-product.js
+++ js/close-product.js
@@ -1,7 +1,100 @@
-const productAside = document.querySelector(".product-aside");
+/********************     WOMEN PAGE      ******************************** */
 
-const closeProductDetails = () => {
-  const closeProduct = document.querySelector(".close-product");
-  productAside.style.display = "none";
-  console.log("pljah");
+const productAsideClosingOW = document.querySelector(
+  "#productAsideOrangeWomen"
+);
+
+const productAsideClosingBW = document.querySelector("#productAsideBlackWomen");
+
+const productAsideClosingBrW = document.querySelector(
+  "#productAsideBrownWomen"
+);
+
+const productAsideClosingOW = document.querySelector(
+  "#productAsideOrangeWomen"
+);
+
+const productAsideClosingBW = document.querySelector("#productAsideBlackWomen");
+
+const productAsideClosingBrW = document.querySelector(
+  "#productAsideBrownWomen"
+);
+
+const closeProductOrangeWomen = () => {
+  const closeProductOW = document.querySelector("#closeProductOrangeWomen");
+  productAsideClosingOW.style.display = "none";
+};
+
+const closeProductBlackWomen = () => {
+  const closeProductBW = document.querySelector("#closeProductBlackWomen");
+  productAsideClosingBW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+/********************     MEN PAGE      ******************************** */
+
+const closeProductOrangeWomen = () => {
+  const closeProductOW = document.querySelector("#closeProductOrangeWomen");
+  productAsideClosingOW.style.display = "none";
+};
+
+const closeProductBlackWomen = () => {
+  const closeProductBW = document.querySelector("#closeProductBlackWomen");
+  productAsideClosingBW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
+};
+
+/********************     OUTLET PAGE      ******************************** */
+
+const closeProductOrangeWomen = () => {
+  const closeProductOW = document.querySelector("#closeProductOrangeWomen");
+  productAsideClosingOW.style.display = "none";
+};
+
+const closeProductBlackWomen = () => {
+  const closeProductBW = document.querySelector("#closeProductBlackWomen");
+  productAsideClosingBW.style.display = "none";
+};
+
+const closeProductBrownWomen = () => {
+  const closeProductBrW = document.querySelector("#closeProductBrownWomen");
+  productAsideClosingBrW.style.display = "none";
 };
