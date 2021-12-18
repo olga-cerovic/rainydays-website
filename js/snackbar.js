5b03cf435c55ac095b881de11de28625b0366a1c
olga-cerovic <olga.cerovic@stud.noroff.no>
2021-12-18 16:37:29 +0100

    html/css
    Updated

--- js/snackbar.js
+++ js/snackbar.js
@@ -1,7 +1,7 @@
 function snackbarFunction() {
-  var x = document.getElementById("snackbar");
-  x.className = "show";
+  const snackbar = document.getElementById("snackbar");
+  snackbar.className = "show";
   setTimeout(function () {
-    x.className = x.className.replace("show", "");
+    snackbar.className = snackbar.className.replace("show", "");
   }, 3000);
 }
